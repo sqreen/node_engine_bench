266c12873c828ce50e4495d5ecda06ffa3fc24ce
vdeturckheim <vlad2t@hotmail.com>
2017-05-03 19:33:12 +0200

    remove 1 target

--- tests/yield.js
+++ tests/yield.js
@@ -6,7 +6,7 @@ function* exec(MAX) {
     }
 }
 
-const max = parseInt(process.argv[2]) || 1000;
+const max = Math.min(parseInt(process.argv[2]) || 1000, 10000);
 for (let i = 0; i < max; ++i) {
     const gen = exec(i);
     let val;
