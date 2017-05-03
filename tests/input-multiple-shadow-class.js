012fa4250bc3e757fc021fffe1f5c038c369db50
vdeturckheim <vlad2t@hotmail.com>
2017-05-03 18:07:00 +0200

    bootstrap project

--- tests/input-multiple-shadow-class.js
+++ tests/input-multiple-shadow-class.js
@@ -10,13 +10,11 @@ for (let i = 0; i < max; ++i) {
     target({ value: i }, { value: i });
 }
 
-const max = parseInt(process.argv[2]) || 1000;
 for (let i = 0; i < max; ++i) {
 
     target({ value: i, c2: true }, { value: i, c2: true});
 }
 
-const max = parseInt(process.argv[2]) || 1000;
 for (let i = 0; i < max; ++i) {
 
     target({ value: i }, { value: i, c2: true});
--- tests/input-multiple-types.js
+++ tests/input-multiple-types.js
@@ -10,13 +10,11 @@ for (let i = 0; i < max; ++i) {
     target(i, i);
 }
 
-const max = parseInt(process.argv[2]) || 1000;
 for (let i = 0; i < max; ++i) {
 
     target('' + i, '' + i);
 }
 
-const max = parseInt(process.argv[2]) || 1000;
 for (let i = 0; i < max; ++i) {
 
     target(i, '' + i);
