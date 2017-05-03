15ff4b663aceb843316a5c05726995d8490939a3
vdeturckheim <vlad2t@hotmail.com>
2017-05-03 18:49:01 +0200

    remove 1 target

--- runner.js
+++ runner.js
@@ -6,7 +6,6 @@ module.exports.VERSION = {
     'v8_7.10.0': '/home/vladimir/.nvm/versions/node/v7.10.0/bin/node',
     'v8_7.10.0_ignition': '/home/vladimir/.nvm/versions/node/v7.10.0/bin/node --ignition',
     'v8_v8.0.0-nightly201705029802d466cc': '/home/vladimir/.nvs/nightly/8.0.0-nightly201705029802d466cc/x64/bin/node',
-    'chakra_v8.0.0-nightly20170415265fc0bedc': '/home/vladimir/.nvs/chakracore/8.0.0-pre2/x64/bin/node',
     'chakra_v8.0.0-nightly201705037f33b4cbf3': '/home/vladimir/.nvs/chakracore-nightly/8.0.0-nightly201705037f33b4cbf3/x64/bin/node'
 };
 
@@ -26,5 +25,7 @@ module.exports.timeRun = function (exec, file, arg) {
     const t0 = process.hrtime();
     CP.execSync(command);
     const t1 = process.hrtime(t0); // [seconds, nanoseconds]
-    return t1[0] * 1000 + t1[1]/1e6;
+    const result = t1[0] * 1000 + t1[1]/1e6;
+    console.log('\t\t', result, 'ms');
+    return result;
 };
